6c12d0baeeb4508318c47ca73f0e13deead6c2ca
vikramlc <vikram.chawan121@gmail.com>
2018-12-21 20:36:46 +0530

    Initial Fix for Junit

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -5,12 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SfgPetClinicApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void setUp() throws Exception {
+        assertEquals(true, true);
     }
 
 }
