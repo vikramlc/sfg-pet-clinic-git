5c73e02d1cfde461268a827e75399710a4bddb3a
vikramlc <vikram.chawan121@gmail.com>
2018-12-26 23:46:11 +0530

    Refactored to Common Base Interface. closes #11.

--- pet-clinic-data/src/main/java/com/vikramlc/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/vikramlc/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.vikramlc.sfgpetclinic.services;
 
 import com.vikramlc.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
