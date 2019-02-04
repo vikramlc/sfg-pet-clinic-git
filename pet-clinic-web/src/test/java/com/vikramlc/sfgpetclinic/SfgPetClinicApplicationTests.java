ac2d4ac0e09d1b718dd72590d5d4e998494fffd9
vikramlc <vikram.chawan121@gmail.com>
2019-02-04 23:16:24 +0530

    Upgraded to Junit5. Closes #4.

--- pet-clinic-web/src/test/java/com/vikramlc/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/vikramlc/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,11 @@
 package com.vikramlc.sfgpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.assertEquals;
-
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SfgPetClinicApplicationTests {
 
@@ -17,7 +15,7 @@ public class SfgPetClinicApplicationTests {
 
     @Test
     public void setUp() throws Exception {
-        assertEquals(true, true);
+
     }
 
 }
